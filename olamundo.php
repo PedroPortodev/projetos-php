33c055f191082ec3fe066bfccd3c95d07e82495e
Pedro Henrique de Abreu Porto <portophtinho@gmail.com>
2021-04-09 10:39:42 -0300

    coisas aprendidas em php

---
+++ olamundo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8"/>
+    <title>primeiro exemplo php</title>
+    <style>
+      h2 {
+        color: #80A2FF
+        text-shadow: 1px 1px 1px black;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>testando php</h1>
+      <h2> "olá, </br> mundo" </h2>
+    
+  </body>
+</html>
