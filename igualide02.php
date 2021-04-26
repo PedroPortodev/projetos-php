32525a28ad5e7ae71da487a003b4ebc556b3adf8
Pedro Henrique de Abreu Porto <portophtinho@gmail.com>
2021-04-26 11:39:52 -0300

    Create igualide02.php

---
+++ igualide02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <title>curso de php - cursoemvideo.com</title>
+  </head>
+  <body>
+    <div>
+      <?php
+        $a = 3;
+        $b = "3";
+        $r = ($a == $b)?"sim":"não";
+        echo "as variaveis A e B são iguais? $r";
+      ?>
+    </div>
+  </body>
+</html>
