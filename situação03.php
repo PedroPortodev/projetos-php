a5676414055183d62bc433dd0a1226511c02d565
Pedro Henrique de Abreu Porto <portophtinho@gmail.com>
2021-04-26 12:02:46 -0300

    Update situação03.php

--- situação03.php
+++ situação03.php
@@ -12,6 +12,8 @@
         $nota2 = $_GET["n2"];
         $m = ($nota1+$nota2)/2;
         echo "a média entrae $nota1 e $nota2 é $m </br>";
+        $sit = ($m<6)? "reprovado":"aprovado";
+        echo "a situação do aluno é $sit"
       ?>
     </div>
   </body>
