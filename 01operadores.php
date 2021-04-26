17a57898affaffd455c70159bb79d6681900da5d
Pedro Henrique de Abreu Porto <portophtinho@gmail.com>
2021-04-26 10:40:26 -0300

    Update 01operadores.php

--- 01operadores.php
+++ 01operadores.php
@@ -3,16 +3,17 @@
   <head>
     <meta charset="UTF-8"/>
     <link rel="stylesheet" href="_css/estilo.css"/>
-    <title></title>
+    <title>curso de php - cursoemvideo.com</title>
   </head>
   <body>
     <div>
       <?php
-        $preco = $_GET["p"];
-        echo "o preço do produto e $preco";
-        $preco += $preco*10/100;
-        echo "</br>E o novo preco com 10% de aumento será R$ " . 
-        number_format($preco, 2);
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];
+        $tipo = $_GET["op"];
+        echo "os valores passados foram $n1 e $n2 </br>";
+        $r = ($tipo == "s") ? $n1+$n2 : $n1*$n2;
+        echo "o resultedo sera $r";
       ?>
     </div>
   </body>
