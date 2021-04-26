3d0e0b2997811dbfd0d358f3c0749f8eb1e34dc5
Pedro Henrique de Abreu Porto <portophtinho@gmail.com>
2021-04-26 11:50:57 -0300

    Create situação03.php

---
+++ situação03.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <title>curso de php - cursoemvideo.com</title>
+  </head>
+  <body>
+    <div>
+      <?php
+        $nota1 = $_GET["n1"];
+        $nota2 = $_GET["n2"];
+        $m = ($nota1+$nota2)/2;
+        echo "a média entrae $nota1 e $nota2 é $m </br>";
+      ?>
+    </div>
+  </body>
+</html>
