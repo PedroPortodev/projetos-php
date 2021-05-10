decc348af5dda746ba83039ac4d47f1ddf4c10de
Pedro Henrique de Abreu Porto <portophtinho@gmail.com>
2021-05-10 12:28:52 -0300

    estrutura de repetição

---
+++ aula11/exercicio01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $n = 10;
+        while ($n >= 1) {
+          echo $n . "</br>";
+          $n--;
+        }
+    ?>
+</div>
+</body>
+</html>
+ 
